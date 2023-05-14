b554a0cfe391aac498f6fc896b0e3b7ad6071daf
Hrudanand <63167093+hrudaniyal@users.noreply.github.com>
2023-05-14 04:19:53 +0000

    add vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,13 @@ import "@fontsource/roboto/700.css";
 import theme from "../styles/theme";
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
-
+import { Analytics } from '@vercel/analytics/react';
 export default function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
